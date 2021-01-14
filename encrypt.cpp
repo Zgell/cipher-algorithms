449cb496029761322fb909e79b366e0074ca3211
Zgell <zgellner2001@gmail.com>
2021-01-13 22:01:13 -0700

    Added part of caesar cipher encryption

--- encrypt.cpp
+++ encrypt.cpp
@@ -2,6 +2,11 @@
 #include <string>
 #include "encrypt.h"
 
-std::string caesar_encrypt(std::string input) {
+std::string caesar_encrypt(std::string input, int key) {
     std::string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789,.!?";
+    chars.append(chars);  // Double the string length to simulate index wrap-around
+    int input_length = input.length();
+    for (int i = 0; i < input_length - 1; i++) {
+        // Find character of input in "chars" string, move it along by "key" indices, replace char
+    }
 }
