01bd8610f79d6d6cee3fa6e3b2c4efd08a086af3
Zgell <zgellner2001@gmail.com>
2021-01-13 21:52:50 -0700

    Added encryption source files

---
+++ encrypt.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+#include "encrypt.h"
+
+std::string caesar_encrypt(std::string input) {
+    std::string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789,.!?";
+}
